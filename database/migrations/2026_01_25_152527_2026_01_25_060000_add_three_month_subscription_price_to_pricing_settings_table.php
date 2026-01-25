a1de4dee21aceb1b72d3a3ec6b477a2168ee80a2
hwy-23db <hwyyy22@gmail.com>
2026-01-25 22:34:06 +0630

    add three month in pricing

--- database/migrations/2026_01_25_152527_2026_01_25_060000_add_three_month_subscription_price_to_pricing_settings_table.php
+++ database/migrations/2026_01_25_152527_2026_01_25_060000_add_three_month_subscription_price_to_pricing_settings_table.php
@@ -8,17 +8,21 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::table('pricing_settings', function (Blueprint $table) {
-            $table->decimal('three_month_subscription_price', 10, 2)
-                ->default(240000)
-                ->after('monthly_subscription_price');
-        });
+         if (!Schema::hasColumn('pricing_settings', 'three_month_subscription_price')) {
+            Schema::table('pricing_settings', function (Blueprint $table) {
+                $table->decimal('three_month_subscription_price', 10, 2)
+                    ->default(240000)
+                    ->after('monthly_subscription_price');
+            });
+        }
     }
 
     public function down(): void
     {
-        Schema::table('pricing_settings', function (Blueprint $table) {
-            $table->dropColumn('three_month_subscription_price');
-        });
+           if (Schema::hasColumn('pricing_settings', 'three_month_subscription_price')) {
+            Schema::table('pricing_settings', function (Blueprint $table) {
+                $table->dropColumn('three_month_subscription_price');
+            });
+        }
     }
 };
